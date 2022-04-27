43c3c7700b7688149a0c3f06d527fec8d2b4e015
SJKed <simonjurstedtskolan@gmail.com>
2022-04-27 08:03:04 +0200

    Added some name setting functionality

--- app.js
+++ app.js
@@ -6,17 +6,22 @@ const io = require('socket.io')(server)
 const path = require('path')
 
 app.use(express.static(path.join(__dirname + '/public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 io.on('connection', socket => {
-    // console.log('Some client connected')
-
+    console.log('Some client connected with the ID: ' + socket.id)
     socket.on('chat', message => {
-        // console.log('From client: ', message)
+        console.log('From client: ', message)
         io.emit('chat', message)
     })
+    socket.on("setName", name => {
+        console.log("name:" + name)
+        socket.emit('setName', name)
+        //redirect to chat page
+        socket.emit('redirect', '/chat.html')
+    })
 })
-
-
 
 server.listen(port, () => {
     console.log(`Server running on port: ${port}`)
--- public/script.js
+++ public/script.js
@@ -1,18 +1,28 @@
 const socket = io()
 
+const setNameForm = document.querySelector('.set-name-form')
+const nameInput = document.querySelector('.name-input')
+
 const chat = document.querySelector('.chat-form')
 const Input = document.querySelector('.chat-input')
 const chatWindow = document.querySelector('.chat-window')
+
+setNameForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const name = nameInput.value
+    console.log("name:" + name)
+    socket.emit('setName', name)
+});
+
+socket.on('redirect', url => {
+    window.location.href = url
+})
 
 chat.addEventListener('submit', event => {
     event.preventDefault()
     socket.emit('chat', Input.value)
     Input.value = ''
 });
-
-socket.on('chat', message => {
-    console.log('From server: ', message)
-})
 
 const renderMessage = message => {
     const div = document.createElement('div')
@@ -22,5 +32,6 @@ const renderMessage = message => {
 }
 
 socket.on('chat', message => {
+    console.log('From server: ', message)
     renderMessage(message)
 })
